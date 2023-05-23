b4fe7c27a5b41c85f8b7b9c6b0f5c280014c6986
GianAndreaSechi <sechi90@hotmail.it>
2023-05-23 17:02:45 +0200

    Add basic project

---
+++ src/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+//API Routes of application
+Route::prefix('api/v1')->group(function () {
+    Route::get('/ping', function () {
+        return json_encode(array('ping'=> now()));
+    });
+});
+
